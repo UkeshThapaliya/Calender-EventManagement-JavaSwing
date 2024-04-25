f507dec2cba2d8e3bdd58ef7c0ea827c832e369f
UT404 <ukeshthapaliya10@gmail.com>
2024-04-25 09:05:49 +0545

    add component and event

--- src/Calender_manage.java
+++ src/Calender_manage.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
 
 public class Calender_manage extends JFrame {
     private List<Event> events = new ArrayList<>();
@@ -17,7 +22,42 @@ public class Calender_manage extends JFrame {
         setTitle("Event Management Calender");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
+        initComponents();
+    }
+
+    private void initComponents() {
+        listModel = new DefaultListModel<>();
+        eventList = new JList<>(listModel);
+        addButton = new JButton("Add Event");
+        editButton = new JButton("Edit Event");
+        deleteButton = new JButton("Delete Event");
+
+        setLayout(new BorderLayout());
+        JPanel controlPanel = new JPanel(new FlowLayout());
+        controlPanel.add(addButton);
+        controlPanel.add(editButton);
+        controlPanel.add(deleteButton);
+
+        add(new JScrollPane(eventList), BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Event newEvent = createNewEvent();
+                if (newEvent != null) {
+                    events.add(newEvent);
+                    updateEventList();
+                }
+            }
+        });
+
+    }
+    private Event createNewEvent() {
+     return null;
+    }
+
+
+    private void updateEventList() {
     }
 }
-
 
